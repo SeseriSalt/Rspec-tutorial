54c8d83e45f25170ea4b6c1f9253da1ab9020e03
Shodai Yada <shodai.yada@techouse.jp>
2022-06-07 14:47:04 +0900

    Complete Setup

--- everydayrails-rspec-jp-2022-01-untested/config/application.rb
+++ everydayrails-rspec-jp-2022-01-untested/config/application.rb
@@ -11,6 +11,16 @@ module Projects
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    #RSpecを標準のテストにするやつ
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false, #ビュースペックを作成しない→代わりにシステムスペックでUIをテスト
+                       helper_specs: false, #ヘルパーファイル用のスペックを作成しない
+                       routing_specs: false #ルーティンぐのスペックを作成しない
+      g.factory_bot false
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
